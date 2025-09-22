9bc588dfe7db662af046c2175cea3d10798c87ea
istiak <istiak@gmail.com>
2025-09-22 23:19:42 +0600

    add icon

--- src/utils/menuItems.tsx
+++ src/utils/menuItems.tsx
@@ -7,55 +7,54 @@ export const adminDashboardData = [
         icon: "ri-dashboard-line",
     },
     {
-        id: 4,
+        id: 2,
         title: "Manage Users",
         name: "Manage Users",
         path: "/users",
-        icon: "ri-user-settings-line", 
+        icon: "ri-team-line",
     },
     { 
-        id: 5,
+        id: 3,
         title: "Projects",
         name: "Projects",
         path: "/projects",
-        icon: "ri-user-settings-line", 
+        icon: "ri-folder-4-line",
     },
     { 
-        id: 2,
+        id: 4,
         title: "Products",
         name: "Products",
         path: "/products",
-        icon: "ri-user-settings-line", 
+        icon: "ri-shopping-bag-3-line",
     },
     {
-        id: 3,
+        id: 5,
         title: "Profile",
         name: "Profile",
         path: "/profile",
-        icon: "ri-user-line", 
+        icon: "ri-user-line",
     },
 ];
 
-export const userDashboardDate = [
+export const userDashboardData = [
     {
         id: 1,
         title: "Dashboard",
         name: "Dashboard",
         path: "/dashboard",
-        icon: "ri-bar-chart-line", 
+        icon: "ri-bar-chart-line",
     },
     {
-        id: 3,
+        id: 2,
         title: "Profile",
         name: "Profile",
         path: "/profile",
-        icon: "ri-user-line", 
+        icon: "ri-user-line",
     },
 ];
 
-
 export const getRoleWiseMenu = (role: string) => {
     if (role === "Admin") return adminDashboardData;
-    if (role === "User") return userDashboardDate;
+    if (role === "User") return userDashboardData;
     return [];
 };
